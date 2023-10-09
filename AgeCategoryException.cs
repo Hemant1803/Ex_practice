49ec7f1cd395dd7a4a1e78d6e4e4d5eec9d2253f
Hemant1803 <hshemantsharma445@gmail.com>
2023-10-09 23:25:14 +0530

    Exception Handling

---
+++ AgeCategoryException.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.Serialization;
+
+namespace Exception_Handling
+{
+    [Serializable]
+    internal class AgeCategoryException : Exception
+    {
+        public AgeCategoryException(string? message) : base(message)
+        {
+        }
+    }
+    public class Age
+    {
+        public static string AgeCategory(int age)
+        {
+            if (age >= 1 && age <= 14)
+            {
+                return "Children";
+            }
+            else if (age >= 15 && age <= 24)
+            {
+                return "Youth";
+            }
+            else if (age >= 25 && age <= 64)
+            {
+                return "Adults";
+            }
+            else if (age >= 65)
+            {
+                return "Seniors";
+            }
+            else
+            {
+                throw new AgeCategoryException("Invalid age provided. Age must be a positive integer.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the person's age: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new AgeCategoryException("Age cannot be empty.");
+                }
+
+                if (!int.TryParse(input, out int age))
+                {
+                    throw new AgeCategoryException("Invalid age format. Age must be a positive integer.");
+                }
+
+                string category = Age.AgeCategory(age);
+                Console.WriteLine($"Category: {category}");
+            }
+            catch (AgeCategoryException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+        }
+    }
+}
